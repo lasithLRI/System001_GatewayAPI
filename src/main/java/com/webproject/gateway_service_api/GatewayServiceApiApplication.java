7ee8eb013492628893261e0cf660a1741d56ddd2
Ishan <dakshinaishan88@gmail.com>
2024-05-24 21:22:24 +0530

    initial commit

---
+++ src/main/java/com/webproject/gateway_service_api/GatewayServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.gateway_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApiApplication.class, args);
+	}
+
+}
